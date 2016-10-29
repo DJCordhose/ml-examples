c3b43d0ec41631f974183d1f199e97e005304e0d
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2016-10-29 21:36:58 +0200

    completed dirty data sets

--- src/kap2/step2_preprocess.py
+++ src/kap2/step2_preprocess.py
@@ -10,7 +10,7 @@ print(df)
 
 # missing values, line 83, second field just not there
 # invalid value, line 58, third field is 'n/a'
-# mismatching units, line 2 convert fourth field (petal width) into cm (from mm)
+# mismatching units, convert fourth field (petal width) into cm (from mm)
 # outliers (bad data), line 144, missing decimal dot in first field (should have been 5.8, not 58)
 # duplicates, line 51, 101
 # spelling mistakes: line 50, Iris-setsoa
