2d28c31e28e67d67d68dc0ef027cdbd02e9c30b8
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2016-10-31 17:11:04 +0100

    more notebooks

--- src/kap2/step2_preprocess.py
+++ src/kap2/step2_preprocess.py
@@ -8,7 +8,7 @@ df = pd.read_csv('../../datasets/Iris/iris_dirty.csv',
                  names=['sepal length', 'sepal width', 'petal length', 'petal width', 'class'])
 
 # missing values, line 83, second field just not there
-# invalid value, line 58, third field is 'n/a'
+# (invalid value, line 58, third field is 'n/a')
 # mismatching units, convert fourth field (petal width) into cm (from mm)
 # outliers (bad data), line 144, missing decimal dot in first field (should have been 5.8, not 58)
 # duplicates, line 51, 101
